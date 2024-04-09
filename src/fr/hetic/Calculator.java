67a055834d2a57fce2f6a1e9170166f1b324b804
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-04-09 16:09:18 +0200

    refactor(Execrice_1): Refactor calculator by adding methods

--- src/fr/hetic/Calculator.java
+++ src/fr/hetic/Calculator.java
@@ -2,23 +2,24 @@ package fr.hetic;
 
 public class Calculator {
     public static void main(String[] args) throws Exception {
-
         Integer number_1 = Integer.parseInt(args[0]);
         Integer number_2 = Integer.parseInt(args[1]);
         String operator = args[2];
 
-        switch(operator) {
-            case "+":
-                System.out.println("The result is " + (number_1 + number_2));
-                break;
-            case "-":
-                System.out.println("The result is " + (number_1 - number_2));
-                break;
-            case "*":
-                System.out.println("The result is " + (number_1 * number_2));
-                break;
-            default:
-                throw new Exception("Operator not allowed");
-        }
+        showResult(calculate(number_1, number_2, operator));
+
+    }
+
+    public static Integer calculate(Integer number_1, Integer number_2, String operator) throws Exception {
+        return switch (operator) {
+            case "+" -> number_1 + number_2;
+            case "-" -> number_1 - number_2;
+            case "*" -> number_1 * number_2;
+            default -> throw new Exception("Operator not allowed");
+        };
+    }
+
+    public static void showResult(Integer result) {
+        System.out.println("The result is " + result);
     }
 }
